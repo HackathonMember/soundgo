7db412be9e7044248298fabe03f22896bd12fb7e
平井優菜 <hiraiyuina@Y-2.local>
2024-11-08 23:36:20 +0900

    activity components

--- frontend/src/components/Activity.jsx
+++ frontend/src/components/Activity.jsx
@@ -1,28 +1,52 @@
 import React from "react";
-import { StyleSheet, View, Text, SafeAreaView } from "react-native";
+import { StyleSheet, View, Text, SafeAreaView, Image } from "react-native";
+
+const sampleImage = require("../../assets/cat1.png");
 
 const Activity = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcomeText}>number of sounds collected</Text>
-      <Text style={styles.welcomeText}>the Day I started</Text>
-      <Text style={styles.welcomeText}>the Day!</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+
+        <Image source={sampleImage} style={styles.image} />
+
+        <View style={{ height: 32 }} />
+
+        <Text style={styles.profileText}>profile</Text>
+        <Text style={styles.profileText}>Name                     Nekon</Text>
+
+        <View style={{ height: 32 }} />
+
+        <Text style={styles.activityText}>activity</Text>
+        <Text style={styles.activityText}>number of sounds collected          7</Text>
+        <Text style={styles.activityText}>the Day I started                         10/02</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#eee",
     justifyContent: "center",
     alignItems: "center",
     padding: 16,
   },
-  welcomeText: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#6cdeeb",
+  image: {
+    width: 200,  // 画像の幅（適宜調整）
+    height: 200, // 画像の高さ（適宜調整）
+  },
+  profileText: {
+    fontSize:12,
+    fontFamily:"Hiragino Mincho ProN",
+    color: "#287aa1",
+    margin:12,
+  },
+  activityText: {
+    fontSize:12,
+    fontFamily:"Hiragino Mincho ProN",
+    color: "#287aa1",
+    margin: 12,
   },
 });
 
